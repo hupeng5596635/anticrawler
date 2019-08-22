944596effe62cde09937c3d68d38bbade9ef7e43
peng.hu02 <peng.hu02@bianlifeng.com>
2019-08-22 23:03:29 +0800

    test in github

---
+++ src/main/java/com/test/web/TestController.java
@@ -0,0 +1,13 @@
+package com.test.web;
+
+/**
+ * @program: anticrawler
+ *
+ * @description: test
+ *
+ * @author: peng.hu02
+ *
+ * @create: 2019-08-22 22:53
+ **/
+public class TestController {
+}
